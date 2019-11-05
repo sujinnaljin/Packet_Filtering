be943e64211876fe1fc754607162f212b5818782
강수진 <rkdthd1234@naver.com>
2019-11-05 14:57:42 +0900

    time 기록 추가

--- socket_pthread/socket_with_pthread.c
+++ socket_pthread/socket_with_pthread.c
@@ -1,3 +1,11 @@
+//
+//  main.c
+//  MYMY2
+//
+//  Created by 강수진 on 2019/11/04.
+//  Copyright © 2019 강수진. All rights reserved.
+//
+
 #include "stdio.h"
 #include "stdlib.h"
 #include "pthread.h"
@@ -20,7 +28,9 @@ void* mysub(void *portNum){
     int clientSocket; //client socket 만들어서 fd 반환된 값,
     struct sockaddr_in serverAddr;
     char buff[BUFF_SIZE];
-
+    struct timeval tv;
+    struct tm *tm;
+    
     //소켓 생성. AF_INET 은 IPv4 Internet protocols
     clientSocket = socket(AF_INET, SOCK_STREAM, 0);
     
@@ -39,26 +49,36 @@ void* mysub(void *portNum){
     
     //connect and error check
     if(connect(clientSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) != -1) {
-        printf("Client : Connect to server\n");
+        printf("Client [PORT %d] : Connect to server\n", myPortNum);
     } else {
-        printf("Client : Cannot connect to server\n");
+        printf("Client [PORT %d] : Cannot connect to server\n", myPortNum);
         exit(0);
-     }
-     
+    }
+    
     char outputFileName[10];
     sprintf(outputFileName, "%d.txt", myPortNum);
     FILE *outputFile;
     outputFile = fopen(outputFileName,"w");
-   
+    printf("file open \n");
     
-    //todo 이부분 고쳐야함. 시간 추가해서 쓰기
-    for(int j =0; j<5; j++) {
+    while(1){
         //읽기
         read(clientSocket, buff, BUFF_SIZE);
-        printf("[PORT %d] : %ld %s\n", myPortNum, strlen(buff), buff);
+        // added by euncho 2019.11.04.
+        
+        // GET TIME
+        gettimeofday(&tv, NULL);
+        tm = localtime(&tv.tv_sec);
+        // add end
+        
         //파일에 작성
-        fprintf(outputFile, "%ld %s\n", strlen(buff), buff);
+        // modified by euncho 2019.11.04.
+        // print time in file
+        printf("received packet. Press Ctrl+C to stop \n");
+        fprintf(outputFile, "%02d:%02d:%d.%03ld %ld %s \n", tm->tm_hour, tm->tm_min, tm->tm_sec, (tv.tv_usec)/1000, strlen(buff), buff);
+        
     }
+    // modify end
     fclose(outputFile);
     return 0;
 }
